7d8eb4b3f7b942031e6e57446954f0f32d47347d
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-09-04 15:13:49 +0900

    Add files via upload

--- common_script1.js
+++ common_script1.js
@@ -35,8 +35,8 @@ mycanvas.style.touchAction = "none";    //キャンバスをタッチ時スク�
 
 
 //スマホ操作時、左端からスワイプした際ブラウザバッグしないようにする・iphoneで長押し時拡大鏡が出ないようにする
-//document.getElementById("canvas1").addEventListener("touchmove",(event)=>{event.preventDefault();},{passive:false});    
-document.addEventListener("touchmove",(event)=>{event.preventDefault();},{passive:false});    
+document.getElementById("canvas1").addEventListener("touchmove",(event)=>{event.preventDefault();},{passive:false});    
+
 
 
 //マウスホイールイベント　カメラのズーム値を変更
