a99289836267fad88d37e8b2958e332d9a78e6a9
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-09-11 12:54:24 +0900

    Add files via upload

--- henkaiten/common_script2.js
+++ henkaiten/common_script2.js
@@ -2,10 +2,10 @@
 
 //detail要素のidを 'mydetail', summaryタグの要素をsummary_labelとする
 
-let counter_detail = 0;
+// let counter_detail = 0;
 
-document.getElementById('mydetail').addEventListener('toggle',(target)=>{
-    if(counter_detail%2==0) document.getElementById('summary_label').textContent = '説明を閉じる'
-    else  document.getElementById('summary_label').textContent = '説明を開く';
-    counter_detail ++;
-});
+// document.getElementById('mydetail').addEventListener('toggle',(target)=>{
+//     if(counter_detail%2==0) document.getElementById('summary_label').textContent = '説明を閉じる'
+//     else  document.getElementById('summary_label').textContent = '説明を開く';
+//     counter_detail ++;
+// });
